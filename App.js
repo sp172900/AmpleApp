09e21996df241563fbdda59aa57838f3d2b7374a
sp172900 <65318960+sp172900@users.noreply.github.com>
2020-08-14 16:05:25 -0400

    Update App.js

--- App.js
+++ App.js
@@ -8,8 +8,6 @@ import FindResourcesScreen from './screens/FindResourcesScreen';
 
 export default function App() {
 
-  // react native app
-
   const [option, setOption] = useState('');
   const [location, setLocation] = useState('');
   const [goToHome, setGoToHome] = useState(false);
